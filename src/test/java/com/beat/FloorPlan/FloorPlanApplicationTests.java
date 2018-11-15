802b14625575fdd5daa4fa6a5a5bbc4746e10c89
tuarender <tuarender005@gmail.com>
2018-11-16 03:03:59 +0700

    vfirst commint

---
+++ src/test/java/com/beat/FloorPlan/FloorPlanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beat.FloorPlan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FloorPlanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
