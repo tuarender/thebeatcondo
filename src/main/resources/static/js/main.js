38b6c12d2ce36d5241bbc5f5bf4c7c89ca12b8e6
tuarender <tuarender005@gmail.com>
2018-11-16 19:30:35 +0700

    resize img

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,6 @@
 var floorMenu = $('.btn.floorMenu');
 var roomTypeArea = $('.roomTypeArea');
 var printType = $('#printType');
-var reservedIcon = $('#reservedIcon');
 var roomTypeImg = $('#roomTypeImg');
 var printImg = $('#printImg');
 
@@ -24,8 +23,7 @@ $(document).ready(function() {
 		roomTypeImg.attr('src','../images/floorPlan/'+building+'/'+type+'.jpg');
 		printImg.attr('src','../images/floorPlan/'+building+'/'+type+'.jpg');
 		$('#price').html(price);
-		setReservedIcon(building, floor, type);
-		
+	
 		$('#roomTypeModal').modal('toggle');
 	});
 	
@@ -33,51 +31,7 @@ $(document).ready(function() {
 		$('#roomTypeModal').modal('toggle');
 		window.print();
 	});
-	
-	reservedIcon.click(function(){
-		var building = this.dataset.building;
-		var floor = this.dataset.floor;
-		var type = this.dataset.type;
-		var localStorageKey = building + '-' + floor + '-' + type;
-		
-		var reserved = getReserved(localStorageKey);
-		if (reserved !== null && reserved === "true") {
-			removeReserved(localStorageKey);
-		}
-		else{
-			setReserved(localStorageKey);
-		}
-	});
 });
-
-function setReservedIcon(building, floor, type) {
-	var localStorageKey = building + '-' + floor + '-' + type;
-	var reserved = getReserved(localStorageKey);
-
-	if (reserved !== null && reserved === "true") {
-		setReserved(localStorageKey);
-	} else {
-		removeReserved(localStorageKey);
-	}
-	
-	reservedIcon.attr('data-building', building);
-	reservedIcon.attr('data-floor', floor);
-	reservedIcon.attr('data-type', type);
-}
-
-function setReserved(localStorageKey){
-	localStorage.setItem(localStorageKey, "true");
-	reservedIcon.removeClass("far").addClass("fas").addClass("reserved");
-}
-
-function removeReserved(localStorageKey){
-	localStorage.setItem(localStorageKey, "false");
-	reservedIcon.removeClass("reserved").removeClass("fas").addClass("far");
-}
-
-function getReserved(localStorageKey){
-	return localStorage.getItem(localStorageKey);
-}
 
 function activateLink(floor) {
 	$('.linkMapped').hide();
